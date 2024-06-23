954f903643765d7f13c6a37838d6a72483345f5e
elhamy786 <elham.afzali1383@gmail.com>
2024-06-23 22:52:06 +0430

    Re-fix script.js file issues

--- script.js
+++ script.js
@@ -10,16 +10,15 @@ document.getElementById('check-btn').addEventListener('click', function() {
   if (textInput === '') { alert('Please input a value');
     return;
   }
-    // Remove non-alphanumeric characters and convert to lowercase
+  // Remove non-alphanumeric characters and convert to lowercase
   const cleanedInput = textInput.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
   const reversedInput = cleanedInput.split('').reverse().join('');
 
-    if (cleanedInput === reversedInput) {
+  if (cleanedInput === reversedInput) {
     resultElement.textContent = `${textInput} is a palindrome`;
     correctGif.style.display = 'block';
     incorrectGif.style.display = 'none';
-  } 
-  else {
+  } else {
     resultElement.textContent = `${textInput} is not a palindrome`;
     correctGif.style.display = 'none';
     incorrectGif.style.display = 'block';
