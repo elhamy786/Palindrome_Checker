38c19548e7c74b25815ac5f29f2c618de20af844
elhamy786 <elham.afzali1383@gmail.com>
2024-06-24 07:49:09 +0430

    Resolve linters error

--- script.js
+++ script.js
@@ -2,18 +2,16 @@
   const textInput = document.getElementById('text-input').value;
   const resultElement = document.getElementById('result');
   document.addEventListener('DOMContentLoaded', () => {
-      document.getElementById('correct-gif').style.display = 'none';
-      document.getElementById('incorrect-gif').style.display = 'none';
+    document.getElementById('correct-gif').style.display = 'none';
+    document.getElementById('incorrect-gif').style.display = 'none';
   });
-  
   const correctGif = document.getElementById('correct-gif');
   const incorrectGif = document.getElementById('incorrect-gif');
-  
   if (textInput === '') {
     alert('Please input a value'); 
     return;
   }
-   const cleanedInput = textInput.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  const cleanedInput = textInput.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
   const reversedInput = cleanedInput.split('').reverse().join('');
 
   if (cleanedInput === reversedInput) {
